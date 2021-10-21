8a1e7625d1884006cfda9dbc692eeae3bb1ad2ea
Amir Rahim <amir.rahim@kcl.ac.uk>
2021-10-21 17:32:35 +0100

    Commit Migrations

--- microblogs/models.py
+++ microblogs/models.py
@@ -1,9 +1,9 @@
+from django.core.validators import RegexValidator
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
-    username = models.CharField(
-        max_length=30
+    username = models.CharField(max_length=30,
         unique=True,
         validators=[RegexValidator(
                 regex=r'^@\w{3,}$',
