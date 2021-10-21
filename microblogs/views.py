e68b204e6e3eea58ea300e3795eb01fcbf5c6a70
Amir Rahim <amir.rahim@kcl.ac.uk>
2021-10-21 19:16:13 +0100

    Log Out Functionality

--- microblogs/views.py
+++ microblogs/views.py
@@ -1,4 +1,5 @@
-from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import redirect, render
 from .forms import LogInForm, SignUpForm
 
@@ -15,8 +16,13 @@ def log_in(request):
             if user is not None:
                 login(request, user)
                 return redirect('feed')
+        messages.add_message(request, messages.ERROR, "The Credentials provided were invalid!")
     form = LogInForm()
     return render(request, 'log_in.html', {'form': form})
+
+def log_out(request):
+    logout(request)
+    return redirect('home')
 
 def home(request):
     return render(request, 'home.html')
